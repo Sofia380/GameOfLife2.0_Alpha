67f2e908a5a4021c3552cfccafdad9c88f0c6d9a
3ae8al0 <sofia.kisarina@gmail.com>
2022-04-11 20:40:39 +0500

    Revert "Create MessageBox in SaveGameWindows"
    This reverts commit 9a8647c5bc2ec0391cda05ae4552d3766fa09054.

--- GameOfLife2.0_Alpha/Variables.cs
+++ GameOfLife2.0_Alpha/Variables.cs
@@ -28,8 +28,7 @@ namespace GameOfLife2._0_Alpha
         public static bool[,] saveFiguresData;
         //
         public static bool[,] fieldSaved;
-        public static bool startSavedGame = false;
-        public static bool checkedCratedField = false;
+
 
     }
     public class GameS
